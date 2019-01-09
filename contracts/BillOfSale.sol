f0d9b97acf22023d7cd57a59ebd792f5d1255858
Joshma91 <josh.ma91@gmail.com>
2019-01-08 22:22:39 -0800

    add contract

---
+++ contracts/BillOfSale.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+contract BillOfSale {
+  address public seller;
+  address public buyer;
+  string public descr;
+  uint public price;
+
+  function recordContract(
+    string _descr,
+    uint _price,
+    address _seller,
+    address _buyer
+  ) public {
+    descr = _descr;
+    price = _price;
+    seller = _seller; 
+    buyer = _buyer;
+  }
+
+  function () public payable { }
+
+  function confirmReceipt() public {
+    require(msg.sender == buyer, "only buyer can confirm");
+    require(this.balance == price, "purchase price must be funded");
+    seller.transfer(address(this).balance);
+  }
+}
