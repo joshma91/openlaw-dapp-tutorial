90e84a1ba04b9c79290ee9b4e4d87e369712fa22
Joshma91 <josh.ma91@gmail.com>
2019-01-21 22:33:01 -0800

    clean up unused files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,4 @@ var BillOfSale = artifacts.require("./BillOfSale.sol");
 
 module.exports = async function(deployer) {
   await deployer.deploy(BillOfSale);
-  // const contract = await BillOfSale.deployed()
-  // await contract.recordContract("testing description", 100000000000000, "0x04Bb2058E3cfC31721d50DceF96C576C761b38c0", "0x04Bb2058E3cfC31721d50DceF96C576C761b38c0")
-};
+ };
